9d06fbe840a317a50a0424a004b0889c0e94918c
Jeffrey Varner <jdv27@cornell.edu>
2025-01-27 09:30:38 -0500

    Lab
    Lab

--- labs/week-2/L2b/Include.jl
+++ labs/week-2/L2b/Include.jl
@@ -17,9 +17,11 @@ using JLD2
 using CSV
 using FileIO
 using LinearAlgebra
+using Statistics
 
 # load my codes -
 include(joinpath(_PATH_TO_SRC, "Types.jl"));
 include(joinpath(_PATH_TO_SRC, "Factory.jl"));
 include(joinpath(_PATH_TO_SRC, "Network.jl"));
 include(joinpath(_PATH_TO_SRC, "Handler.jl"));
+include(joinpath(_PATH_TO_SRC, "Compute.jl"));
