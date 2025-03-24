b8530acc01a1d1efb13ad14ec3122cae697cf24b
Jeffrey Varner <jdv27@cornell.edu>
2025-03-24 18:21:27 -0400

    L10b
    L10b

--- labs/week-10/L10b/src/Compute.jl
+++ labs/week-10/L10b/src/Compute.jl
@@ -7,31 +7,30 @@ function simulate(model::MySimpleBoltzmannMachineModel, sₒ::Vector{Int};
 
     number_of_neurons = length(sₒ);
     S = zeros(Int, number_of_neurons, T);
-    turns = zeros(Int, T);
-    energy = zeros(Float64, T);
     is_ok_to_stop = false; # flag to stop the simulation
+    h = zeros(Float64, number_of_neurons); # input to the neurons
 
     # package initial state -
     s = copy(sₒ); # initial state
-    energy[1] = -(1/2)*dot(s, W*s) - dot(b, s); # compute the energy of the initial state
     S[:, 1] .= s; # store the initial state in the S matrix
-    turns[1] = 1; # store the time step
 
     # main loop -
     t = 2;
     while (is_ok_to_stop == false)
 
-        # process each neuron -
+        # Compute the input to each neuron -
         for i ∈ 1:number_of_neurons
-            hᵢ = dot(W[i, :], s) + b[i]; # compute the input for node i -
-            pᵢ = 1 / (1 + exp(-2 * β * hᵢ));  # compute the probability of flipping the i-th bit
-            flag = Bernoulli(pᵢ) |> rand; # build a bernoulli random variable, sample it
-            s[i] = flag == 1 ? 1 : -1; # flip the i-th bit
+            h[i] = dot(W[i, :], s) + b[i]; # compute the input for node i -
+        end
+
+        # update the state of the neurons -
+        for i ∈ 1:number_of_neurons
+            pᵢ = (1 / (1 + exp(-2 * β * h[i])));  # compute the probability of flipping the i-th bit
+            flag = Bernoulli(pᵢ) |> rand # flip the i-th bit with probability pᵢ
+            s[i] = flag == 1 ? 1 : -1; # flip the i-th bit for the *next* state
         end
         
-        energy[t] = -(1/2)*dot(s, W*s) - dot(b, s); # compute and store the energy of the current state
         S[:, t] .= copy(s); # store the current state in the S matrix
-        turns[t] = t; # store the time step
         
         if (t == T)
             is_ok_to_stop = true; # stop the simulation
