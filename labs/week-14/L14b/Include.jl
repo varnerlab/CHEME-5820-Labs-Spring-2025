8a1d9267d4708128396cb2424ce35e5e9d0111d5
Jeffrey Varner <jdv27@cornell.edu>
2025-04-22 07:02:44 -0400

    data
    data

---
+++ labs/week-14/L14b/Include.jl
@@ -0,0 +1,17 @@
+# setup paths -
+const _ROOT = @__DIR__;
+const _PATH_TO_DATA = joinpath(_ROOT, "data");
+const _PATH_TO_SRC = joinpath(_ROOT, "src");
+
+# if we are missing any packages, install them -
+using Pkg;
+if (isfile(joinpath(_ROOT, "Manifest.toml")) == false) # have manifest file, we are good. Otherwise, we need to instantiate the environment
+    Pkg.activate("."); Pkg.resolve(); Pkg.instantiate(); Pkg.update();
+end
+
+# load external packages -
+using Flux
+using NNlib
+using Statistics
+using LinearAlgebra
+using OneHotArrays
